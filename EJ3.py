f8c16fd1a7f559660f565096f310a49867812fec
JQ3417 <95460965+JQ3417@users.noreply.github.com>
2021-12-02 23:15:48 -0600

    Delete EJ3.py

--- EJ3.py
+++
@@ -1,5 +0,0 @@
-cantidad = float(input("Escribe la cantidad a invertir: "))
-interes = float(input("Escribe el interes: "))
-años = int(input("Escibre el número de años: "))
-
-print(f'Ganancias: {cantidad * interes/100 + años}')
